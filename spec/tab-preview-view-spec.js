0a88d59b65466311ecbb711e3802e7edfe935157
Williams Medina <williams.medinaa@gmail.com>
2016-03-17 22:39:02 -0400

    Initial files

---
+++ spec/tab-preview-view-spec.js
@@ -0,0 +1,9 @@
+var NodeBugView;
+
+NodeBugView = require('../lib/tab-preview-view');
+
+describe("NodeBugView", function() {
+  return it("has one valid test", function() {
+    return expect("life").toBe("easy");
+  });
+});
