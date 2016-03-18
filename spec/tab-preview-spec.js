48b7b173b9e75e42a4dbf1021e510afd91ff9f16
Williams Medina <williams.medinaa@gmail.com>
2016-03-18 00:09:21 -0400

    load package on init

--- spec/tab-preview-spec.js
+++ spec/tab-preview-spec.js
@@ -1,46 +1,7 @@
-var NodeBug;
+'use strict';
 
-NodeBug = require('../lib/tab-preview');
+const TavPreview = require('../lib/tab-preview');
 
 describe("NodeBug", function() {
-  var activationPromise, ref, workspaceElement;
-  ref = [], workspaceElement = ref[0], activationPromise = ref[1];
-  beforeEach(function() {
-    workspaceElement = atom.views.getView(atom.workspace);
-    return activationPromise = atom.packages.activatePackage('tab-preview');
-  });
-  return describe("when the tab-preview:toggle event is triggered", function() {
-    it("hides and shows the modal panel", function() {
-      expect(workspaceElement.querySelector('.tab-preview')).not.toExist();
-      atom.commands.dispatch(workspaceElement, 'tab-preview:toggle');
-      waitsForPromise(function() {
-        return activationPromise;
-      });
-      return runs(function() {
-        var nodeBugElement, nodeBugPanel;
-        expect(workspaceElement.querySelector('.tab-preview')).toExist();
-        nodeBugElement = workspaceElement.querySelector('.tab-preview');
-        expect(nodeBugElement).toExist();
-        nodeBugPanel = atom.workspace.panelForItem(nodeBugElement);
-        expect(nodeBugPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'tab-preview:toggle');
-        return expect(nodeBugPanel.isVisible()).toBe(false);
-      });
-    });
-    return it("hides and shows the view", function() {
-      jasmine.attachToDOM(workspaceElement);
-      expect(workspaceElement.querySelector('.tab-preview')).not.toExist();
-      atom.commands.dispatch(workspaceElement, 'tab-preview:toggle');
-      waitsForPromise(function() {
-        return activationPromise;
-      });
-      return runs(function() {
-        var nodeBugElement;
-        nodeBugElement = workspaceElement.querySelector('.tab-preview');
-        expect(nodeBugElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'tab-preview:toggle');
-        return expect(nodeBugElement).not.toBeVisible();
-      });
-    });
-  });
+
 });
--- spec/tab-preview-view-spec.js
+++
@@ -1,9 +0,0 @@
-var NodeBugView;
-
-NodeBugView = require('../lib/tab-preview-view');
-
-describe("NodeBugView", function() {
-  return it("has one valid test", function() {
-    return expect("life").toBe("easy");
-  });
-});
